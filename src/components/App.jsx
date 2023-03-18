afe655d7464f4a8a4221caf5514244a37f9c405a
Dina-Ursula <dinatyan26@gmail.com>
2023-03-19 00:15:24 +0200

    change-export

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-function App() {
+export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -72,5 +72,3 @@ function App() {
     </>
   );
 }
-
-export default App;
